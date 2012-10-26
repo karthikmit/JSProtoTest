1ff502cecacc6db62a5e1daf09b8fd5924c4ae18
karthikeyan.s <karthikeyan.s@directi.com>
2012-10-26 17:13:01 +0530

    For review

--- TwitterFeed.js
+++ TwitterFeed.js
@@ -5,39 +5,81 @@
  * Time: 9:28 AM
  * To change this template use File | Settings | File Templates.
  */
-
 FeedsQueue = {
     feeds : [],
+    feedsCache : [],
 
-    getTweets : function(twitterId) {
+    getTweets : function(twitterId, onSuccess) {
         var numTweets = 5;
-        this.collectTwitterFeed(twitterId, numTweets);
+        if(this.isExists(twitterId) !== true) {
+            this.collectTwitterFeed(twitterId, numTweets, onSuccess);
+        }
     },
 
-    collectTwitterFeed : function(twitterId, numTweets) {
-        $.ajax({
-                url: 'http://api.twitter.com/1/statuses/user_timeline.json/',
-                type: 'GET',
-                dataType: 'jsonp',
-                data: {
-                    screen_name: twitterId,
-                    include_rts: true,
-                    count: numTweets,
-                    include_entities: true
-                },
+    removeTweets : function(twitterId, onSuccess) {
+        this.feeds = $.grep(this.feeds, function(element) {
+            return element.key !== twitterId.toLowerCase();
+        });
+        onSuccess();
+    },
 
-                success: function(data, textStatus, xhr){
-                    // append tweets into feeds queue.
-                    for (var i = 0; i < data.length; i++) {
-                        FeedsQueue.feeds.push(data[i]);
+    isExists : function(twitterId) {
+        var isExistsFlag = false;
+        $.grep(this.feeds, function(e) {
+            isExistsFlag = e.key == twitterId.toLowerCase();
+        });
+        alert(isExistsFlag + "");
+        return isExistsFlag;
+    },
+
+    isExistsInCache : function(twitterId) {
+        var isExistsFlag = false;
+        $.grep(this.feedsCache, function(e) {
+            isExistsFlag = e.key == twitterId.toLowerCase();
+        });
+
+        return isExistsFlag;
+    },
+
+    retrieveFromCache : function(twitterId) {
+        return this.feedsCache.filter(function(element) {
+            return element.key === twitterId.toLowerCase();
+        })[0];
+    },
+
+    collectTwitterFeed : function(twitterId, numTweets, onSuccess) {
+        if(this.isExistsInCache(twitterId) !== true) {
+            $.ajax({
+                    url: 'http://api.twitter.com/1/statuses/user_timeline.json/',
+                    type: 'GET',
+                    dataType: 'jsonp',
+                    data: {
+                        screen_name: twitterId,
+                        include_rts: true,
+                        count: numTweets,
+                        include_entities: true
+                    },
+
+                    success: function(data, textStatus, xhr){
+                        // append tweets into feeds queue.
+                        FeedsQueue.feeds.push({key:data[0].user.screen_name.toLowerCase(), value:data});
+                        FeedsQueue.feedsCache.push({key:data[0].user.screen_name.toLowerCase(), value:data});
+
+                        onSuccess();
+                    },
+                    error : function()
+                    {
+                        alert("error");
                     }
-                },
-                error : function()
-                {
-                    alert("error");
                 }
-            }
-        )
+            )
+        }
+        else {
+            var keyValObj = this.retrieveFromCache(twitterId);
+            FeedsQueue.feeds.push(keyValObj);
+            //FeedsQueue.feeds.push({key:keyValObj.key, value : keyValObj.value});
+            onSuccess();
+        }
     }
 }
 
